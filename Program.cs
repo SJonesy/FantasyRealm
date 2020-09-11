f67d443756efcf4dc251ee5c88ef33a665510a7c
Stephen Jones <stephenisflawless@gmail.com>
2020-09-10 19:41:06 -0500

    init

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace FantasyRealm
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
